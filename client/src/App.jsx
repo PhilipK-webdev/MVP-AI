83ca528b9cd9dab2eea5df8bf45f04c0c035a334
PhilipK-webdev <philipk179@gmail.com>
2024-11-13 07:26:09 +0200

    adding libraries to the client, create proxy between the server and the client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    //getConfig();
+  }, []);
+
+  // const getConfig = async () => {
+  //   const response = await fetch("/api/config");
+  //   const result = await response.json();
+  //   console.log(result);
+  // };
   return (
     <>
       <div>
@@ -29,7 +38,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
